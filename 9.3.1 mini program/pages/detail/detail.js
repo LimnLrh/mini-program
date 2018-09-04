654c57d3f6a1b63cfedd33bb6a5265a38eed331b
LimnLrh <2397254099@qq.com>
2018-09-04 19:07:42 +0800

    mini program

---
+++ 9.3.1 mini program/pages/detail/detail.js
@@ -0,0 +1,40 @@
+// pages/detail/detail.js
+import {fetch} from "../../utils/util.js"
+Page({
+  data: {
+    bookId:"",
+    bookData:{},
+    isLoading:false
+  },
+  onLoad: function (options) {
+    console.log(options)
+    this.setData({
+      bookId:options.id
+    })
+    this.getData()
+  },
+  getData (){
+    this.setData({
+      isLoading:true
+    })
+    fetch.get(`/book/${this.data.bookId}`).then(res=>{
+      console.log(res)
+      this.setData({
+        bookData:res,
+        isLoading:false
+      })
+    }).catch(err=>{
+      this.setData({
+        isLoading:false
+      })
+    })
+  },
+  jumpCatalog(){
+    wx.navigateTo({
+      url: `/pages/catalog/catalog?id=${this.data.bookId}`,
+    })
+  },
+  onShareAppMessage: function () {
+  
+  }
+})
